57214f397b7b82896f849a31dc79c6d6ba45322e
Mutoi01 <pngando9@gmail.com>
2022-06-19 09:24:40 +0300

    commit tree 747476aaf3d69ad020746d07c3b68e682fba02e0

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+const len = word.length;
+
+for(i = 0; i < len / 2; i++){
+  if(word[i] !== word[len - 1 - i]){
+    return false
+  }
 }
+return true
+}
+
 
 /* 
   Add your pseudocode here
